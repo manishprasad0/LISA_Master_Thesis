2e4b0162b882311eff2fb950423ef1430c56b047
Manish Prasad <manish.prasad0511@gmail.com>
2025-06-15 11:47:36 +0200

    pre-merger function and mcmc

--- tools/time_freq_likelihood.py
+++ tools/time_freq_likelihood.py
@@ -12,11 +12,13 @@ from typing import Any, Tuple, Optional, List
 # RIGHT NOW MAKING THIS ONLY FOR A AND E CHANNELS, NOT T CHANNEL
 # class TimeFreqLikelihood is like the AnalysisContainer class in lisatools, but it is specifically designed for time-frequency likelihood calculations.
 class TimeFreqLikelihood:
-    def __init__(self, data_t, wave_gen, nperseg = 15000):
+    def __init__(self, data_t, wave_gen, nperseg = 15000, dt_full=5.0):
         self.data_t = data_t
         self.wave_gen = wave_gen
+        self.dt_full = dt_full  # This is the time step for the full data, not the STFT
+        self.time_before_merger = None
         
-        # stft
+        # All the following quantities are for after the STFT is calculated
         self.dt = None
         self.nperseg = nperseg  # default value, can be changed later
         self.f = None
@@ -24,16 +26,27 @@ class TimeFreqLikelihood:
         self.Zxx_data_A = None
         self.Zxx_data_E = None
         self.sens_mat_new = None
+
+    def pre_merger(self, time_before_merger, t_ref, t_array):
+        self.time_before_merger = time_before_merger
+
+        cutoff_time = t_ref - time_before_merger
+        cutoff_index = np.searchsorted(t_array, cutoff_time)
+        self.cutoff_index = cutoff_index
+
+        data_t_truncated = self.data_t[:, :cutoff_index]
+        self.data_t = data_t_truncated
     
-    def get_stft_of_data(self, dt=5.0, include_sens_kwargs=False):
+    def get_stft_of_data(self, include_sens_kwargs=False):
         """
         Calculate the Short-Time Fourier Transform (STFT) of the data and set up the frequency and time arrays.
         Parameters:
         - dt: Time step for the data, default is 5.0 seconds. Different from self.dt which is the time step for the STFT.
         - include_sens_kwargs: If True, include sensitivity matrix parameters in the sensitivity matrix calculation.
         """
-        f, t, Zxx_data_A = sp.signal.stft(self.data_t[0], fs=1/dt, nperseg=self.nperseg)
-        f, t, Zxx_data_E = sp.signal.stft(self.data_t[1], fs=1/dt, nperseg=self.nperseg)
+        
+        f, t, Zxx_data_A = sp.signal.stft(self.data_t[0], fs=1/self.dt_full, nperseg=self.nperseg)
+        f, t, Zxx_data_E = sp.signal.stft(self.data_t[1], fs=1/self.dt_full, nperseg=self.nperseg)
         
         self.f = f
         self.df = f[1] - f[0]  # frequency bin width
@@ -81,6 +94,10 @@ class TimeFreqLikelihood:
         template_f = template_f[:2] # remove T channel
         template_t = np.fft.irfft(template_f, axis=-1)
 
+        if self.time_before_merger is not None:
+            # Truncate the template to the same length as the data
+            template_t = template_t[:, :self.cutoff_index]
+
         Zxx_temp_A = sp.signal.stft(template_t[0], fs=1/self.dt, nperseg=self.nperseg)[2]
         Zxx_temp_E = sp.signal.stft(template_t[1], fs=1/self.dt, nperseg=self.nperseg)[2]
 
@@ -90,3 +107,17 @@ class TimeFreqLikelihood:
         dd = self.get_dd()
 
         return (dh - hh / 2.0 - dd / 2.0) * self.dt
+
+    def plot_spectrogram(self, max_freq = 0.1, min_freq = 1e-4):
+
+        max_freq_idx = np.searchsorted(self.f, max_freq)
+        min_freq_idx = np.searchsorted(self.f, min_freq)
+
+        plt.figure()
+        plt.pcolormesh(self.t, self.f[min_freq_idx:max_freq_idx], np.abs(self.Zxx_data_A[min_freq_idx:max_freq_idx]), vmin=0, 
+                    vmax= np.max(np.abs(self.Zxx_data_A[min_freq_idx:max_freq_idx]))/10)
+        plt.yscale('log')
+        plt.title('STFT Magnitude')
+        plt.ylabel('Frequency [Hz]')
+        plt.xlabel('Time [sec]')
+        plt.colorbar()
